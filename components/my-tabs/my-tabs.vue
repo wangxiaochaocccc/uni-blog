0b317b27893dc99f7c09b5c4b107d31ce0c5e672
wangchao <wangxiaochaocccc@163.com>
2023-03-14 22:08:26 +0800

    feat:my-tabs

---
+++ components/my-tabs/my-tabs.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class="my-tabs-container">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-tabs",
+		props: {
+			config:{
+				type:Object,
+				default:()=>{
+					return {}
+				}
+			},
+			tabData: {
+				type:Array,
+				default:()=>{
+					return []
+				}
+			},
+			chooseIndex:{
+				type:Number,
+				default:0
+			}
+		}
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
