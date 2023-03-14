6b3ed5e7091ecd4e1a220f23caebe72fffa44397
wangchao <wangxiaochaocccc@163.com>
2023-03-14 18:11:42 +0800

    feat:my-search组件

---
+++ components/my-search/my-search.vue
@@ -0,0 +1,48 @@
+<template>
+	<view class="my-search-container">
+		<view class="container">
+			<image class="icon" src="@/static/images/search.png" mode=""></image>
+			<text class="placeholder">{{placeholderText}}</text>
+		</div>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-search",
+		props: {
+			placeholderText: {
+				type:String,
+				default:'搜索'
+			}
+		},
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+.my-search-container {
+	height: 36px;
+	.container {
+		height: 36px;
+		border: 1px solid #c9c9c9;
+		display: flex;
+		align-items: center;
+		border-radius: 15px;
+		padding: 0 10px;
+		margin: auto;
+		.icon {
+			width: 20px;
+			height: 20px;
+		}
+		.placeholder {
+			color: #666;
+			margin-left: 8px;
+		}
+	}
+}
+</style>
--- pages/hot/hot.vue
+++ pages/hot/hot.vue
@@ -1,6 +1,9 @@
 <template>
 	<view class="hot-container">
 		<image class="logo" src="@/static/images/logo.png" mode="aspectFit"></image>
+		<view class="search-box">
+			<my-search></my-search>
+		</view>
 	</view>
 </template>
 
@@ -20,6 +23,10 @@
 	.logo {
 		width: 100%;
 		height: 80px;
+	}
+	.search-box {
+		padding: 0 20px;
+		margin-bottom: 15px;
 	}
 }
 </style>
