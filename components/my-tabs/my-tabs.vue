1942cf03312060a8edb79ab21bfbfde0da0ab84e
wangchao <wangxiaochaocccc@163.com>
2023-03-15 00:17:57 +0800

    feat:my-tabs滑块滑动逻辑

--- components/my-tabs/my-tabs.vue
+++ components/my-tabs/my-tabs.vue
@@ -4,9 +4,10 @@
 			<scroll-view class="scroll-view" scroll-x="true"  scroll-with-animation>
 				<view class="tabs-item">
 					<block v-for="(item,index) in tabData" :key="item.id">
-						<view class="item" @click="handleClickTabs(index)" :class="{activeTab:activeIndex===index }">{{item.label}}</view>
+						<view :id="'_tab_'+index" class="item" @click="handleClickTabs(index)" :class="{activeTab:activeIndex===index }">{{item.label}}</view>
 					</block>
 				</view>
+				<view class="underline" :style="'transform:translateX('+slider.left+'px)'"></view>
 			</scroll-view>
 		</view>
 	</view>
@@ -35,18 +36,62 @@
 		},
 		data() {
 			return {
-				activeIndex:-1 //当前激活项
+				activeIndex:-1, //当前激活项
+				slider: {
+					left:0
+				},
+				tabList:[] //本地维护一个tabs数据对象
 			};
 		},
 		methods:{
 			handleClickTabs(index){
 				this.activeIndex = index
+				// 滑块距离
+				this.tabToActived()
+			},
+			updateTabsUnderline(){
+				const data = this.tabList
+				if(!data.length) return
+				// 获取dom固定写法
+				const query = uni.createSelectorQuery().in(this)
+				
+				data.forEach((item,index)=>{
+					query
+					.select('#_tab_'+index)
+					.boundingClientRect((res)=>{
+						item.slider = {
+							left:res.left + (res.width-24)/2
+						}
+						
+						// 立即执行一次
+						if(index === data.length-1) {
+							this.tabToActived()
+						}
+					})
+					.exec()
+					
+				})
+			},
+			tabToActived(){
+				const index = this.activeIndex
+				this.slider = {
+					left:this.tabList[index].slider.left
+				}
 			}
 		},
 		watch:{
 			chooseIndex:{
 				handler(val){
 					this.activeIndex = val
+				},
+				immediate:true
+			},
+			tabData: {
+				handler(val){
+					this.tabList = val
+					setTimeout(()=>{
+						this.updateTabsUnderline()
+					},0)
 				},
 				immediate:true
 			}
@@ -62,6 +107,7 @@
 		width: 100%;
 		height: 45px;
 		.scroll-view {
+			position: relative;
 			width: 100%;
 			height: 45px;
 			white-space: nowrap;
@@ -78,7 +124,17 @@
 					color: #fa942a;
 				}
 			}
+			.underline {
+				width: 24px;
+				height: 2px;
+				background-color: #fa942a;
+				border-radius: 3px;
+				position: absolute;
+				bottom: 20px;
+				transition: .5s;
+			}
 		}
+		
 	}
 }
 </style>
