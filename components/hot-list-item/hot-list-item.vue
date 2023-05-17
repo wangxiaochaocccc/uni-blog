9b28da1f622f74185c0e0e739ee2f052c18e3a0b
wangchao <wangxiaochaocccc@163.com>
2023-05-18 02:24:53 +0800

    feat:hot-list-item基本样式

--- components/hot-list-item/hot-list-item.vue
+++ components/hot-list-item/hot-list-item.vue
@@ -2,7 +2,7 @@
 	<view class="item-container">
 		<view class="item-box">
 			<view class="item-left">
-				<imgage src="@/static/images/ranking-1.png" class="ranking-bg"></imgage>
+				<image src="@/static/images/ranking-1.png" class="ranking-bg"></image>
 				<text class="ranking-text">1</text>
 			</view>
 			<view class="item-right">
@@ -33,6 +33,55 @@
 	}
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.item-container {
+	padding: 8px 10px;
+	.item-box {
+		display: flex;
+		.item-left {
+			width: 20px;
+			height: 20px;
+			position: relative;
+			.ranking-bg {
+				width: 20px;
+				height: 20px;
+			}
+			.ranking-text {
+				position: absolute;
+				top: 50%;
+				left: 50%;
+				transform: translate(-55%,-55%);
+				font-size: 12px;
+				color: #333;
+			}
+		}
+		.item-right {
+			width: 100%;
+			font-size: 14px;
+			.item-title {
+				margin-bottom: 5px;
+				font-weight: bold;
+			}
+			.item-desc {
+				font-size: 12px;
+				color: #333;
+			}
+			.item-bottom {
+				width: 100%;
+				display: flex;
+				justify-content: space-between;
+				font-size: 12px;
+				margin-top: 5px;
+				.item-hot {
+					display: flex;
+					align-items: center;
+					.hot-icon {
+						width: 20px;
+						height: 20px;
+					}
+				}
+			}
+		}
+	}
+}
 </style>
